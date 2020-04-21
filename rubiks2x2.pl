ca9dfa440c9bf6beb1d7b185bcd73005ff062927
Adrian Wijaya <adrianwijaya100@gmail.com>
2020-04-21 13:27:18 +0700

    Add: Show cubic in matrix form

--- rubiks2x2.pl
+++ rubiks2x2.pl
@@ -136,3 +136,19 @@ rotate(
 		O2,O4,O1,O3  % back
 	)
 ).
+
+show(cube(W1,W2,W3,W4,Y1,Y2,Y3,Y4,G1,G2,G3,G4,B1,B2,B3,B4,R1,R2,R3,R4,O1,O2,O3,O4)) :-
+	print_rubik([[' ',' ',' ',' ',O1,' ',O2],
+	      [' ',' ',' ',' ',O3,' ',O4],
+		  [G1,' ',G2,' ',W1,' ',W2,' ',B1,' ',B2],
+		  [G3,' ',G4,' ',W3,' ',W4,' ',B3,' ',B4],
+		  [' ',' ',' ',' ',R1,' ',R2],
+		  [' ',' ',' ',' ',R3,' ',R4],
+		  [' ',' ',' ',' ',Y1,' ',Y2],
+		  [' ',' ',' ',' ',Y3,' ',Y4]]).
+
+print_rubik([]).
+print_rubik([H|T]) :- write_line(H), nl, print_rubik(T).
+
+write_line([]).
+write_line([H|T]) :- write(H), write_line(T).
