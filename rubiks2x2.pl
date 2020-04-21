71c7396f64f35f56f194518420d73ee90bc5f3c7
Adrian Wijaya <adrianwijaya100@gmail.com>
2020-04-21 13:38:06 +0700

    Fix: redundant rule

--- rubiks2x2.pl
+++ rubiks2x2.pl
@@ -7,6 +7,8 @@
 % E is front
 % F is back
 % %
+:- include('show.pl').
+
 solved(cube(A,A,A,A,B,B,B,B,C,C,C,C,D,D,D,D,E,E,E,E,F,F,F,F)).
 
 % Main logic
@@ -136,21 +138,3 @@ rotate(
 		O2,O4,O1,O3  % back
 	)
 ).
-
-show(cube(W1,W2,W3,W4,Y1,Y2,Y3,Y4,G1,G2,G3,G4,B1,B2,B3,B4,R1,R2,R3,R4,O1,O2,O3,O4)) :-
-	print_rubik([
-		  [' ',' ',' ',' ',O1,' ',O2],
-	      [' ',' ',' ',' ',O3,' ',O4],
-		  [G1,' ',G2,' ',W1,' ',W2,' ',B1,' ',B2],
-		  [G3,' ',G4,' ',W3,' ',W4,' ',B3,' ',B4],
-		  [' ',' ',' ',' ',R1,' ',R2],
-		  [' ',' ',' ',' ',R3,' ',R4],
-		  [' ',' ',' ',' ',Y1,' ',Y2],
-		  [' ',' ',' ',' ',Y3,' ',Y4]
-	]).
-
-print_rubik([]).
-print_rubik([H|T]) :- write_line(H), nl, print_rubik(T).
-
-write_line([]).
-write_line([H|T]) :- write(H), write_line(T).
